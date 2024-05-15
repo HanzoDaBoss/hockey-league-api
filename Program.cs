c6e6a069c1301da3addc5f0adc7d0d3c330136b7
HanzoDaBoss <hanif.uddz@gmail.com>
2024-05-15 15:17:57 +0100

    Added createdatroute for return value of POST request

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using HockeyLeague.Api.Dtos;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+const string GetTeamEndpointName = "GetTeam";
+
 List<TeamDto> teams = [
     new (
         1,
@@ -18,7 +20,8 @@ List<TeamDto> teams = [
 
 app.MapGet("teams", () => teams);
 
-app.MapGet("teams/{id}", (int id) => teams.Find(team => team.Id == id));
+app.MapGet("teams/{id}", (int id) => teams.Find(team => team.Id == id))
+    .WithName(GetTeamEndpointName);
 
 app.MapPost("teams", (CreateTeamDto newTeam) =>
 {
@@ -27,6 +30,9 @@ app.MapPost("teams", (CreateTeamDto newTeam) =>
         newTeam.Name,
         newTeam.Division
     );
+    teams.Add(team);
+
+    return Results.CreatedAtRoute(GetTeamEndpointName, new { id = team.Id }, team);
 });
 
 app.Run();
